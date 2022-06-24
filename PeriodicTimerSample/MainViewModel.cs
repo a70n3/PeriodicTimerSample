c8e7b688134cb1f019bd7fa55fac5ea92b4cdb53
a70n3 <prohibitous@gmail.com>
2022-06-24 18:18:24 +0800

    Add project files.

---
+++ PeriodicTimerSample/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PeriodicTimerSample
+{
+    public class MainViewModel
+    {
+        private PeriodicTimer _periodicTimer;
+
+        private CancellationTokenSource _token;
+
+        private Task? _timerTask;
+
+        private readonly ObservableCollection<string> _names = new();
+        public MainViewModel(TimeSpan timespan)
+        {
+            _periodicTimer = new(timespan);
+            
+        }
+        public ObservableCollection<string> Names {
+            get => _names;
+        }
+        public void Start()
+        {
+            _token = new();
+            _timerTask = DoWork();
+        }
+        public async Task DoWork()
+        {
+            try
+            {
+                while (await _periodicTimer.WaitForNextTickAsync(_token.Token))
+                {
+                    _names.Add($"Clone Number : {Random.Shared.Next(1, 1000)}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+        }
+        public async Task StopTimer()
+        {
+            if (_timerTask is null)
+            {
+                return;
+            }
+
+            _token.Cancel();
+            await _timerTask;
+            _token.Dispose();
+        }
+    }
+}
---
+++ PeriodicTimerSample/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+
+namespace PeriodicTimerSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(TimeSpan.FromMilliseconds(1000));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Start();
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                try
+                {
+                    await vm.StopTimer();
+                }
+                catch (OperationCanceledException)
+                {
+                    MessageBox.Show("Operation was cancelled by the user");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex}");
+                }
+            }
+        }
+    }
+}
